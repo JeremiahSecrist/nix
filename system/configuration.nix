6f61ac402a882f300a0da267e76b2c5c3e2ad163
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-01-16 13:34:07 -0500

    typo fix on init pass

--- system/configuration.nix
+++ system/configuration.nix
@@ -79,7 +79,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jane = {
     isNormalUser = true;
-    initialPassword = "changeme!!"
+    initialPassword = "changeme!!";
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   };
 
