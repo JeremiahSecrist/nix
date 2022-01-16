24548018b0a7ec96544576711a3531c10b6cd9dd
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-01-16 17:10:19 -0500

    added git
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- system/configuration.nix
+++ system/configuration.nix
@@ -89,6 +89,7 @@
     nano # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     firefox
+    git
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
