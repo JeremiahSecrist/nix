c81dcde2a586ecac93bfc1cd3c9ff3a2b19ab4e7
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-01-16 01:17:08 -0500

    cleaned up code

--- install.sh
+++ install.sh
@@ -16,9 +16,8 @@ Sel-Disk(){
     drives = "${lsblk -d | tail -n+2 | cut -d" " -f1 | tr '\n' ' '}"
     select i in $drives do echo "/dev/${i} selected"; Confirm() ; done
 }
-
+Format-Disk(){
 sgdisk --zap-all /dev/sda
-
 sgdisk --clear \
          --new=1:0:+550MiB --typecode=1:ef00 \
          --new=2:0:+8GiB   --typecode=2:8200 \
@@ -30,3 +29,7 @@ mkfs.fat -F 32 -n EFI /dev/sda1
 mkswap -L swap -f /dev/sda2 
 mkfs.ext4 /dev/sda3 --label=nixos -f
 mount /dev/disk/by-label/nixos /mnt
+}
+
+## init phase
+Sel-Dsk()
