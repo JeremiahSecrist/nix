72d012d58dd34fefac47de772db87bee3ef152a8
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-01-16 01:09:39 -0500

    seldsk wip

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 Sel-Disk(){
     lsblk -f
     drives = "${lsblk -d | tail -n+2 | cut -d" " -f1 | tr '\n' ' '}"
-    select i in $drives do echo $i; done
+    select i in $drives do echo "/dev/${i} selected"; done
 }
 
 sgdisk --zap-all /dev/sda
@@ -12,7 +12,7 @@ sgdisk --clear \
          --new=1:0:+550MiB --typecode=1:ef00 \
          --new=2:0:+8GiB   --typecode=2:8200 \
          --new=3:0:0       --typecode=3:8300 \
-           /dev/sda
+           /dev/${DSKSLC}
 
 #Partition the drive and create subvolumes
 mkfs.fat -F 32 -n EFI /dev/sda1 
