fb4e2877c5a99b997101ea88a85c91e9bb6b4085
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-01-17 04:57:23 -0500

    portainer on host os is anoying

--- system/configuration.nix
+++ system/configuration.nix
@@ -72,7 +72,7 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 9443 ];
+  networking.firewall.allowedTCPPorts = [ 22 9443 443 ];
   networking.firewall.allowedUDPPorts = [  ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
--- system/docker.nix
+++ system/docker.nix
@@ -5,7 +5,7 @@
     virtualisation.oci-containers.backend = "docker";
     virtualisation.oci-containers.containers."portainer" = {
      image = "portainer/portainer-ce:2.11.0";
-     ports = ["9443:9443"];
+     ports = ["0.0.0.0:9443:9443"];
      volumes = [ "portainer_data:/data" "/var/run/docker.sock:/var/run/docker.sock" ];
     };
 
