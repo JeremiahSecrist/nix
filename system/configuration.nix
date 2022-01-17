4db361f793bfb3747afd90d06d89a8b424f2f053
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-01-17 04:11:03 -0500

    docker backend simpler
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- system/configuration.nix
+++ system/configuration.nix
@@ -51,7 +51,8 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     nano # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget 
+    wget
+    docker
     # firefox 
     git 
   ];
