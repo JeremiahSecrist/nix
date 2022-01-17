5068ef037624d872b726211aa57ea09b78905689
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-01-17 04:46:45 -0500

    fixed portainer port issue
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- system/docker.nix
+++ system/docker.nix
@@ -5,7 +5,7 @@
     virtualisation.oci-containers.backend = "docker";
     virtualisation.oci-containers.containers."portainer" = {
      image = "portainer/portainer-ce:2.11.0";
-     ports = ["127.0.0.1:9443:9443"];
+     ports = ["9443:9443"];
      volumes = [ "portainer_data:/data" "/var/run/docker.sock:/var/run/docker.sock" ];
     };
 
