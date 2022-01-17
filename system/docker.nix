a7c0a4c36484e3c093a308f90ecc176c7f0f9b40
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-01-17 17:49:43 -0500

    systemd service not needed for podman

--- system/docker.nix
+++ system/docker.nix
@@ -12,16 +12,16 @@
      ports = ["0.0.0.0:9443:9443"];
      volumes = [ "portainer_data:/data" "/var/run/docker.sock:/var/run/docker.sock" ];
     };
-    systemd.services.dockerstop = {
-      wantedBy = [ "containerd.service" ]; 
-      before = [ "docker.service" ];
-      after = [ "containerd.service" ];
-      requires = ["containerd.service"];
-      Description = "containerd-shim v2 workaround";
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = "yes";
-        ExecStop = "/bin/sh -c ''${pkgs.docker}/bin/docker kill $(${pkgs.docker}/bin/docker ps -q)''";
-      };
-   };
+#     systemd.services.dockerstop = {
+#       wantedBy = [ "containerd.service" ]; 
+#       before = [ "docker.service" ];
+#       after = [ "containerd.service" ];
+#       requires = ["containerd.service"];
+#       Description = "containerd-shim v2 workaround";
+#       serviceConfig = {
+#         Type = "oneshot";
+#         RemainAfterExit = "yes";
+#         ExecStop = "/bin/sh -c ''${pkgs.docker}/bin/docker kill $(${pkgs.docker}/bin/docker ps -q)''";
+#       };
+#    };
 }
