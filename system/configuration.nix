5d3b0aa9b3cef95baab94136d99cee8d0d3a5c45
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-01-26 22:59:12 -0500

    firefox and flatpaks
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- system/configuration.nix
+++ system/configuration.nix
@@ -70,10 +70,14 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    nano # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    gnome-software
+    nano 
+    firefox
     wget
     git 
   ];
+  xdg.portal.enable = true;
+  services.flatpak.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
