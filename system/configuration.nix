8d165f620231805dc515b300000cea9cd79000c2
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-01-26 22:59:50 -0500

    gnomesoftware
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- system/configuration.nix
+++ system/configuration.nix
@@ -70,7 +70,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    gnome-software
+    gnome.gnome-software
     nano 
     firefox
     wget
