1cc28308b6554a54489fe4cb2301aab8affb832c
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-06-07 12:42:00 -0400

    fixed file local
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ nixos-generate-config --root /mnt
 }
 
 Install_Nix(){
-    pushd /home/nixos/.dotfiles
+    pushd /home/nixos/nix
     sudo nixos-install -I nixos-config=./system/${SYSTEM_NAME}/configuration.nix
     popd
 }
