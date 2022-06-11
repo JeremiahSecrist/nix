f7cc7bda68ea00b3cfdd80be698b6208c76bd760
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-06-11 17:08:05 -0400

    added shas
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- system/laptop/configuration.nix
+++ system/laptop/configuration.nix
@@ -5,7 +5,10 @@
 { config, pkgs, ... }:
 let
   unstable = import
-    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/02ab2e2aa58df6231c319af3ce1af79f3e82e2d8)
+    (builtins.fetchTarball {
+        url = "https://github.com/nixos/nixpkgs/tarball/02ab2e2aa58df6231c319af3ce1af79f3e82e2d8";
+        sha256 = "62d96e502d0f6976809354ca0c6bc070de7f5ccd0082683fcfc9a1b426d90e1e";
+    }; )
     # reuse the current configuration
     { config = config.nixpkgs.config; };
 in
