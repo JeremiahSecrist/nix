cbc8b97ddb447990969348b6ea6ad57e76304591
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-06-11 22:56:51 -0400

    ;
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
                             home-manager.useGlobalPkgs = true;
                             home-manager.useUserPackages = true;
                             home-manager.users.sky = {
-                                import = [ ./hm/sky/home.nix]
+                                import = [ ./hm/sky/home.nix];
                             };
                         };
                     ];
