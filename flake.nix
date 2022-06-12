d97a829c39b8cbac12f0d89009eff3d4d841eb6c
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-06-11 22:49:16 -0400

    addming home manager
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- flake.nix
+++ flake.nix
@@ -1,9 +1,12 @@
 {
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-
+        home-manager = {
+            url = github:nix-community/home-manager;
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
-    outputs = { self, nixpkgs }:
+    outputs = { self, nixpkgs, home-manager, ... }:
         let
             system = "x86_64-linux";
             pkgs = import nixpkgs {
@@ -17,6 +20,13 @@
                     inherit system;
                     modules = [
                         ./system/laptop/configuration.nix
+                        homa-manager.nixosModules.home-manager {
+                            home-manager.useGlobalPkgs = true;
+                            home-manager.useUserPackages = true;
+                            home-manager.users.sky = {
+                                import = [ ./hm/sky/home.nix]
+                            };
+                        };
                     ];
                 };
             };
