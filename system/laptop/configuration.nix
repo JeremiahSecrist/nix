2735c3d3c833d9877ba492e3980dcf8a2d682971
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-06-11 20:38:14 -0400

    vpn support and make
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- system/laptop/configuration.nix
+++ system/laptop/configuration.nix
@@ -64,6 +64,8 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    remake
+    protonvpn-gui
     ecryptfs
     ecryptfs-helper
     nano
