ca216abc2fcd29961d39fc4ae38db15cf85cb33a
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-06-17 12:53:43 -0400

    line spacing and cleanup
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- modules/nixos/u2fLogin/default.nix
+++ modules/nixos/u2fLogin/default.nix
@@ -1,16 +1,16 @@
-_: { config, pkgs, ... }: 
+_: { config, pkgs, lib, ... }: 
 {
+# services.udev.extraRules = ''
+# ACTION!="add|change", GOTO="fido_end"
+# KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0407", TAG+="uaccess"
+# LABEL="fido_end"'';
+services.pcscd.enable = true;
 security = {
     pam = {
       u2f = {
         enable = true;
         control = "sufficient";
         cue = true;
-      };
-      services = {
-        login.u2fAuth = true;
-        gdm.u2fAuth = true;
-        slock.u2fAuth = true;
       };
     };
   };
