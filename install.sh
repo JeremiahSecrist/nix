5524f70940473ff5eaa24b85a702cbe0a0bf3696
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-06-20 23:06:06 -0400

    switching to encrypted lvm boot
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- install.sh
+++ install.sh
@@ -43,12 +43,38 @@ swapon /dev/disk/by-label/swap
 nixos-generate-config --root /mnt
 }
 
+Encrypted_Format_Disk(){
+    # Clear Disk
+    sgdisk --zap-all "${DISK}"
+    Await
+
+    # Create Partitions
+    sgdisk --clear \
+         --new=1:0:+550MiB  --typecode=1:ef00
+         --new=2:0:0        --typecode=2:8300
+    cryptsetup luksFormat   ${DISK}2
+    cryptsetup luksOpen     ${DISK}2 enc-pv
+    lvcreate -L 8G -n swap vg
+    lvcreate -l '100%FREE' -n nixos vg
+
+    # Format Partitions
+    mkfs.fat    -F 32 -n boot ${DISK}1
+    mkfs.ext4   -L nixos /dev/vg/root
+    mkswap      -L swap /dev/vg/swap
+
+    # Mount Partitions
+    mount   /dev/disk/by-label/nixos /mnt
+    mkdir   -p /mnt/boot
+    mount   /dev/disk/by-label/boot /mnt/boot
+    swapon  /dev/disk/by-label/swap
+    
+}
 Install_Nix() {
     nix-install --flake https://github.com/arouzing/nix#${SYSTEM_NAME} --root /mnt
 }
 
 ## init phase
-Format_Disk
+Encrypted_Format_Disk
 lsblk
 Confirm
 Install_Nix
