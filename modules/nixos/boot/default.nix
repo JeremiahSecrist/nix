1a7be879f75c51dff9ed8f2ec63ccced3e928b0f
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-06-20 23:06:29 -0400

    added boot alternative incase issues
    Signed-off-by: arouzing <owner@arouzing.xyz>

---
+++ modules/nixos/boot/default.nix
@@ -0,0 +1,23 @@
+_: { config, pkgs, lib, ... }:
+
+{    
+    # Bootloader.
+    boot.plymouth.enable = true;
+    boot.loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+        timeout = 1;
+    };
+    fileSystems."/" =
+    { device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-label/swap"; }
+    ];
+}
