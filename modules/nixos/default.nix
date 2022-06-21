9c8b929f066d8ca67ddda5e4219c88d54ce60837
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-06-20 23:05:46 -0400

    imported boot and encrypted boot
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,11 +1,13 @@
 inputs:
 
 {
-    common = import ./common inputs;
-    harden = import ./harden inputs;
-    sound = import ./sound inputs;
-    yubikey = import ./yubikey inputs;
-    u2fLogin = import ./u2fLogin inputs;
-    gnomeDesktop = import ./gnomeDesktop inputs;
+    common          =   import ./common         inputs;
+    boot            =   import ./boot           inputs;
+    encryptedBoot   =   import ./encryptedBoot  inputs;
+    harden          =   import ./harden         inputs;
+    sound           =   import ./sound          inputs;
+    yubikey         =   import ./yubikey        inputs;
+    u2fLogin        =   import ./u2fLogin       inputs;
+    gnomeDesktop    =   import ./gnomeDesktop   inputs;
 
 }
