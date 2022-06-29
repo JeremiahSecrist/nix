72d85eb253b02159e04e9230716cbc8c18f33aba
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-06-29 15:18:07 -0400

    considering installing nix on my desktop
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- flake.nix
+++ flake.nix
@@ -38,6 +38,26 @@
                         encryptedBoot
                     ]);
                 };
+                skydesk = lib.nixosSystem {
+                    inherit system;
+                    modules = [
+                        ./system/desktop/configuration.nix
+                        home-manager.nixosModules.home-manager {
+                            home-manager.useGlobalPkgs = true;
+                            home-manager.useUserPackages = true;
+                            home-manager.users.sky = {
+                                imports = [ ./hm/sky/home.nix ];
+                            };
+                        }
+                    ]++ (with self.nixosModules; [
+                        common
+                        sound
+                        yubikey
+                        u2fLogin
+                        gnomeDesktop
+                        encryptedBoot
+                    ]);
+                };
             };
         };
 }
