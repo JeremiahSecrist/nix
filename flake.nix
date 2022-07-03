131911b0ca30e8d128ac24b94b44651a51af8e84
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-07-03 01:34:08 -0400

    I'll figure out unstable later
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- flake.nix
+++ flake.nix
@@ -1,25 +1,15 @@
 {
     inputs = {
-        nixpkgs.url = "nixpkgs/nixos-22.05";
-        unstable.url = "nixpkgs/nixos-unstable"; 
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+        # nixpkgs-unstable.url = "nixpkgs/nixos-unstable"; 
         home-manager = {
             url = github:nix-community/home-manager;
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
-    outputs = { self, nixpkgs, unstable, home-manager, ... }@inputs:
+    outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
         let
             system = "x86_64-linux";
-            overlay = { 
-                pkg-sets = (
-                    final: prev: {
-                        unstable = import inputs.unstable { system = final.system; };
-                        # trunk = import inputs.trunk { system = final.system; };
-                    }
-                );
-            };
-            # Overlays-module makes "pkgs.unstable" available in configuration.nix
-            overlayModule = ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay ]; });
             pkgs = import nixpkgs {
                 inherit system;
                 config.allowUnfree = true;
