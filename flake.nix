8737eddec0236bd9b0b120299abafee388414d9a
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-07-02 22:57:06 -0400

    adding unstable support
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+        nixpkgs-unstable.url = "nixpkgs/nixos-unstable"; 
         home-manager = {
             url = github:nix-community/home-manager;
             inputs.nixpkgs.follows = "nixpkgs";
