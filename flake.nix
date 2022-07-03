c1575b4add0dffbd21a6b529616321c8cfc50924
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-07-02 23:16:53 -0400

    testing out adding unstable packages as overlay
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-        nixpkgs-unstable.url = "nixpkgs/nixos-unstable"; 
+        nixpkgs.url = "nixpkgs/nixos-22.05";
+        unstable.url = "nixpkgs/nixos-unstable"; 
         home-manager = {
             url = github:nix-community/home-manager;
             inputs.nixpkgs.follows = "nixpkgs";
@@ -10,11 +10,10 @@
     outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
         let
             system = "x86_64-linux";
+            overlay = final: prev: { unstable = unstable.legacyPackages.${prev.system}; };
+            # Overlays-module makes "pkgs.unstable" available in configuration.nix
+            overlayModule = ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay ]; });
             pkgs = import nixpkgs {
-                inherit system;
-                config.allowUnfree = true;
-            };
-            pkgs-unstable = import nixpkgs-unstable {
                 inherit system;
                 config.allowUnfree = true;
             };
