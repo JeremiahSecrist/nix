661da5918b850333e64f4131cdeed02a8df70763
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-07-03 01:35:10 -0400

    ree
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
-    outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
+    outputs = { self, nixpkgs, home-manager, ... }@inputs:
         let
             system = "x86_64-linux";
             pkgs = import nixpkgs {
