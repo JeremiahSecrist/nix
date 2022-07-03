57c4c38fc4aef2d8a2cd98494ed870a8305d3928
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-07-02 23:37:09 -0400

    another attempt at unstable pkgs
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- flake.nix
+++ flake.nix
@@ -10,7 +10,14 @@
     outputs = { self, nixpkgs, unstable, home-manager, ... }@inputs:
         let
             system = "x86_64-linux";
-            overlay = final: prev: { unstable = unstable.legacyPackages.${prev.system}; };
+            overlay = { 
+                pkg-sets = (
+                    final: prev: {
+                        unstable = import inputs.unstable { system = final.system; };
+                        # trunk = import inputs.trunk { system = final.system; };
+                    }
+                );
+            };
             # Overlays-module makes "pkgs.unstable" available in configuration.nix
             overlayModule = ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay ]; });
             pkgs = import nixpkgs {
