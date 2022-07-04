14d8bd9b7470a00bef3fe52398f2aada64acace6
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-07-04 15:16:22 -0400

    migrated let to each system
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- flake.nix
+++ flake.nix
@@ -1,26 +1,21 @@
 {
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05-small";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+        nixpkgs-small.url = "github:NixOS/nixpkgs/nixos-22.05-small";
         # nixpkgs-unstable.url = "nixpkgs/nixos-unstable"; 
         home-manager = {
             url = github:nix-community/home-manager;
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
-    outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    outputs = { self, nixpkgs, nixpkgs-small, home-manager, ... }@inputs:
         let
             system = "x86_64-linux";
-            pkgs = import nixpkgs {
-                inherit system;
-                config.allowUnfree = true;
-            };
-            lib = nixpkgs.lib;
         in {
             nixosModules = import ./modules/nixos inputs;
-            # homeModules  = import ./modules/hm inputs;
             nixosConfigurations = {
-                skytop = import ./system/laptop (inputs // { inherit lib system; });
-                cache_server = import ./system/cache_server (inputs // { inherit lib system; });
+                skytop = import ./system/laptop (inputs // { inherit nixpkgs system; });
+                cache_server = import ./system/cache_server (inputs // { inherit nixpkgs-small system; });
             };
         };
 }
