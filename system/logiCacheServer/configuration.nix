7e5c415749049803556da609767c3928e751a3e6
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-07-06 23:49:05 -0400

    setting ip addr
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- system/logiCacheServer/configuration.nix
+++ system/logiCacheServer/configuration.nix
@@ -9,6 +9,12 @@
   
   networking = {
     hostName = "cacheserver"; # Define your hostname.
+    defaultGateway = "192.168.1.254";
+    interfaces.eth0.ipv4.addresses = [ {
+      address = "192.168.1.2";
+      prefixLength = 24;
+    }   
+  ];
     networkmanager.enable = true;
   };
   time.timeZone = "America/Denver";
