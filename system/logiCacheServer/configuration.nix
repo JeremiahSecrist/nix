52fd5a0135c6295a6aa84d10e3435e89af024460
arouzing <593336a4-f160-432d-981d-34f51d9ad98d@anonaddy.me>
2022-07-07 01:30:20 -0400

    a
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- system/logiCacheServer/configuration.nix
+++ system/logiCacheServer/configuration.nix
@@ -54,7 +54,7 @@
     hostName = "cacheserver"; # Define your hostname.
   #   defaultGateway = "192.168.1.254";
   #   nameservers = [ "1.1.1.1" ];
-  #   enableIPv6 = false;
+    enableIPv6 = false;
   #   interfaces.eth0.ipv4.addresses = [ {
   #     address = "192.168.1.2";
   #     prefixLength = 24;
