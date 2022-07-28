73842661ae25b5cae44b7e842b9a14cecdf4fcdb
arouzing <owner@arouzing.xyz>
2022-07-28 01:21:04 -0400

    zsh now sup[port delete!
    Signed-off-by: arouzing <owner@arouzing.xyz>

--- hm/sky/home.nix
+++ hm/sky/home.nix
@@ -61,6 +61,8 @@
             }
             starship_precmd_user_func="set_win_title"
             precmd_functions+=(set_win_title)
+
+            bindkey "^[[3~" delete-char
             '';
             enableCompletion            = true;
             completionInit              = ''
