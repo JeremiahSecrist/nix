299da485f984db356dae00e4461a0a394fae3a9f
arouzing <owner@arouzing.xyz>
2022-07-28 21:38:53 -0400

    updated dnf

--- hm/sky/dconf.nix
+++ hm/sky/dconf.nix
@@ -26,7 +26,7 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/Console" = {
-      font-scale = 1.1;
+      font-scale = 1.2;
     };
 
     "org/gnome/calculator" = {
@@ -51,7 +51,6 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/control-center" = {
-      last-panel = "keyboard";
       window-state = mkTuple [ 980 640 ];
     };
 
@@ -82,10 +81,11 @@ with lib.hm.gvariant;
       enable-hot-corners = false;
       font-antialiasing = "grayscale";
       font-hinting = "slight";
+      gtk-theme = "Adwaita-dark";
     };
 
     "org/gnome/desktop/notifications" = {
-      application-children = [ "org-gnome-console" "gnome-power-panel" "org-gnome-software" "code" "firefox" ];
+      application-children = [ "org-gnome-console" "gnome-power-panel" "org-gnome-software" "code" "firefox" "discord" ];
     };
 
     "org/gnome/desktop/notifications/application/code" = {
@@ -108,6 +108,10 @@ with lib.hm.gvariant;
       application-id = "org.gnome.Console.desktop";
     };
 
+    "org/gnome/desktop/notifications/application/org-gnome-nautilus" = {
+      application-id = "org.gnome.Nautilus.desktop";
+    };
+
     "org/gnome/desktop/notifications/application/org-gnome-software" = {
       application-id = "org.gnome.Software.desktop";
     };
@@ -121,7 +125,7 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/session" = {
-      idle-delay = mkUint32 300;
+      idle-delay = mkUint32 0;
     };
 
     "org/gnome/desktop/wm/keybindings" = {
@@ -194,10 +198,12 @@ with lib.hm.gvariant;
 
     "org/gnome/settings-daemon/plugins/power" = {
       power-button-action = "suspend";
+      sleep-inactive-ac-timeout = 3600;
     };
 
     "org/gnome/shell" = {
       app-picker-layout = "[{'org.gnome.Contacts.desktop': <{'position': <0>}>, 'org.gnome.Weather.desktop': <{'position': <1>}>, 'org.gnome.clocks.desktop': <{'position': <2>}>, 'org.gnome.Maps.desktop': <{'position': <3>}>, 'org.gnome.Calculator.desktop': <{'position': <4>}>, 'simple-scan.desktop': <{'position': <5>}>, 'org.gnome.Settings.desktop': <{'position': <6>}>, 'gnome-system-monitor.desktop': <{'position': <7>}>, 'Utilities': <{'position': <8>}>, 'yelp.desktop': <{'position': <9>}>, 'org.gnome.Calendar.desktop': <{'position': <10>}>, 'org.cryptomator.Cryptomator.desktop': <{'position': <11>}>, 'org.gnome.Extensions.desktop': <{'position': <12>}>, 'firefox.desktop': <{'position': <13>}>, 'cups.desktop': <{'position': <14>}>, 'nixos-manual.desktop': <{'position': <15>}>, 'org.gnome.Software.desktop': <{'position': <16>}>, 'spotify.desktop': <{'position': <17>}>, 'test-tabby.desktop': <{'position': <18>}>, 'org.gnome.TextEditor.desktop': <{'position': <19>}>, 'org.gnome.Tour.desktop': <{'position': <20>}>, 'xterm.desktop': <{'position': <21>}>, 'com.yubico.yubioath.desktop': <{'position': <22>}>}]";
+      command-history = [ "pkill Discord" ];
       disable-user-extensions = true;
       disabled-extensions = [ "dash-to-dock@micxgx.gmail.com" "workspace-indicator@gnome-shell-extensions.gcampax.github.com" ];
       enabled-extensions = [ "appindicatorsupport@rgcjonas.gmail.com" "material-shell@papyelgringo" "auto-move-windows@gnome-shell-extensions.gcampax.github.com" "drive-menu@gnome-shell-extensions.gcampax.github.com" ];
@@ -228,7 +234,6 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/software" = {
-      check-timestamp = mkInt64 1655424175;
       first-run = false;
     };
 
