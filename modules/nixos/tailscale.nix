0bf854096577ba5ed43458c0677cf7b97aeed41c
arouzing <owner@arouzing.xyz>
2022-07-29 21:06:32 -0400

    updated

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -2,4 +2,7 @@ _: { config, pkgs, lib, ... }:
 
 {
     services.tailscale.enable = lib.mkDefault true;
+    # Strict reverse path filtering breaks 
+    # Tailscale exit node use and some subnet routing setups
+    networking.firewall.checkReversePath = lib.mkDefault "loose";
 }
