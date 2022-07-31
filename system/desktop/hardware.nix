b4996216266f1ee3c669904b754332dce4bfdfab
arouzing <owner@arouzing.xyz>
2022-07-30 22:59:12 -0400

    now using zen kernel

--- system/desktop/hardware.nix
+++ system/desktop/hardware.nix
@@ -5,6 +5,8 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_zen;
+
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
