825b6de8c770e9429e6380232a7d656e0b2ce146
arouzing <owner@arouzing.xyz>
2022-07-30 22:55:47 -0400

    cleaned up noise torch

--- hm/sky/noisetorch.nix
+++ hm/sky/noisetorch.nix
@@ -1,22 +1,23 @@
-_: { config, pkgs, lib, ... }: 
+{ config, pkgs, lib, ... }: 
 {
 systemd.user.services = {
-  service-name = {
+    
+  noisetorch = {
     Unit = {
-      Description = "Example description";
-      After="pulseaudio.service";
+      Description       =   "noisetorch";
+      After             =   ["pulseaudio.service"];
     };
 
     Service = {
-        Type="simple";
+        Type            =   "simple";
         RemainAfterExit =   "yes";
-        ExecStart       =   "${pkgs.noisetorch}/bin/noisetorch -i -s $DEVICEID -t 95";
+        ExecStart       =   "${pkgs.noisetorch}/bin/noisetorch -i -s alsa_input.usb-Generic_TONOR_TC40_Audio_Device-00.analog-stereo -t 95";
         ExecStop        =   "${pkgs.noisetorch}/bin/noisetorch -u";
         Restart         =   "on-failure";
-        RestartSec      =   3
+        RestartSec      =   "3";
     };
     Install = {
-        WantedBy="default.target";
+        WantedBy        =   ["default.target"];
     };
   };
 };
