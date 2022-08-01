ef25445e21c41576e71810a89956003eed238c20
arouzing <owner@arouzing.xyz>
2022-08-01 17:31:57 -0400

    now following the propper branch for hm

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
         nixpkgs-small.url = "github:NixOS/nixpkgs/nixos-22.05-small";
         # nixpkgs-unstable.url = "nixpkgs/nixos-unstable"; 
         home-manager = {
-            url = github:nix-community/home-manager;
+            url = github:nix-community/home-manager/release-22.05;
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
@@ -14,9 +14,9 @@
         in {
             nixosModules = import ./modules/nixos inputs;
             nixosConfigurations = {
-                skytop      = import ./system/laptop             (inputs // { inherit system; inherit (nixpkgs)lib; });
-                desksky      = import ./system/desktop             (inputs // { inherit system; inherit (nixpkgs)lib; });
-                cacheServer = import ./system/cacheServer   (inputs // { inherit system; inherit (nixpkgs-small)lib; });
+                skytop      = import ./system/laptop                (inputs // { inherit system; inherit (nixpkgs)lib; });
+                desksky      = import ./system/desktop              (inputs // { inherit system; inherit (nixpkgs)lib; });
+                cacheServer = import ./system/cacheServer           (inputs // { inherit system; inherit (nixpkgs-small)lib; });
                 logiCacheServer = import ./system/logiCacheServer   (inputs // { inherit system; inherit (nixpkgs-small)lib; });
             };
         };
