438c40590afef107c1db5beaa3ef6d6be21edf16
arouzing <owner@arouzing.xyz>
2022-08-01 17:34:49 -0400

    user level gpg agent support

--- hm/sky/home.nix
+++ hm/sky/home.nix
@@ -35,7 +35,15 @@
 
     ];
     };
-    
+    services.gpg-agent = {
+        enable = true;
+        enableSshSupport = true;
+        enableExtraSocket = true;
+        defaultCacheTtl = 34560000;
+        defaultCacheTtlSsh = 34560000;
+        maxCacheTtl = 34560000;
+        maxCacheTtlSsh = 34560000;
+    };
     programs                    = {
         home-manager.enable     = true;
         direnv = {
