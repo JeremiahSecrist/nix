d95dc546fbe2e9c79cb673be36db0104b2f6058c
arouzing <owner@arouzing.xyz>
2022-08-07 15:42:02 -0400

    converting to nixos cache server

--- system/cacheServer/configuration.nix
+++ system/cacheServer/configuration.nix
@@ -5,71 +5,56 @@
     ./hardware.nix
     ./docker.nix
   ];
-  # lollypops.deployment = {
-  #   user = "admin";
-  #   sshOpts = "-A";
-  #   config-dir = "/home/admin/deployment";
-  # };
-  # base packages
   environment.systemPackages = with pkgs; [ htop tailscale ];
+  services.tailscale.enable = true;
 
-  #### temp for testing
+  #### security settings
   boot.kernelPackages = pkgs.linuxPackages_hardened;
   boot.kernel.sysctl."kernel.unprivileged_userns_clone" = true;
-  nix.settings.allowed-users = [ "@users" ];
+  nix.settings.allowed-users = [ "@wheel" ];
   security.lockKernelModules = true;
   security.protectKernelImage = true;
-  security.allowSimultaneousMultithreading = false;
   security.forcePageTableIsolation = true;
-  # This is required by podman to run containers in rootless mode.
-  security.unprivilegedUsernsClone = config.virtualisation.containers.enable;
-  security.virtualisation.flushL1DataCache = "always";
+  ### 
 
-  # Enable strict reverse path filtering (that is, do not attempt to route
-  # packets that "obviously" do not belong to the iface's network; dropped
-  # packets are logged as martians).
-  # boot.kernel.sysctl."net.ipv4.conf.all.log_martians" =   true;
-  # boot.kernel.sysctl."net.ipv4.conf.all.rp_filter" =   "1";
-  # boot.kernel.sysctl."net.ipv4.conf.default.log_martians" =   true;
-  # boot.kernel.sysctl."net.ipv4.conf.default.rp_filter" =   "1";
-
-  # Ignore broadcast ICMP (mitigate SMURF)
-  boot.kernel.sysctl."net.ipv4.icmp_echo_ignore_broadcasts" = true;
-
-  # Ignore incoming ICMP redirects (note: default is needed to ensure that the
-  # setting is applied to interfaces added after the sysctls are set)
-  boot.kernel.sysctl."net.ipv4.conf.all.accept_redirects" = false;
-  boot.kernel.sysctl."net.ipv4.conf.all.secure_redirects" = false;
-  boot.kernel.sysctl."net.ipv4.conf.default.accept_redirects" = false;
-  boot.kernel.sysctl."net.ipv4.conf.default.secure_redirects" = false;
-  boot.kernel.sysctl."net.ipv6.conf.all.accept_redirects" = false;
-  boot.kernel.sysctl."net.ipv6.conf.default.accept_redirects" = false;
-
-  # Ignore outgoing ICMP redirects (this is ipv4 only)
-  boot.kernel.sysctl."net.ipv4.conf.all.send_redirects" = false;
-  boot.kernel.sysctl."net.ipv4.conf.default.send_redirects" = false;
-  ###
   networking = {
     firewall.checkReversePath = "loose";
-    hostName = "cacheserver"; # Define your hostname.
+    hostName = "cache"; # Define your hostname.
     networkmanager.enable = true;
   };
+  services.nix-serve = {
+    enable = true;
+    secretKeyFile = "/var/cache-priv-key.pem";
+    port = 8080;
+  };
 
-  services.tailscale.enable = true;
+  services.nginx = {
+    enable = true;
+    virtualHosts = {
+      # ... existing hosts config etc. ...
+      "cache.local.arouzing.win" = {
+        serverAliases = [ "cache.local.arouzing.win" ];
+        locations."/".extraConfig = ''
+          proxy_pass http://localhost:${
+            toString config.services.nix-serve.port
+          };
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        '';
+      };
+    };
+  };
 
   time.timeZone = "America/New_York";
 
   # Disable wait for network
   systemd.network.wait-online.timeout = 0;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.admin = {
     isNormalUser = true;
-    # initialPassword = "password";
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPM5d9h1r7e4NBTUr5ZSO2dFTCZM3BNa5TKvgjqTJDOw"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCYTCBJMl2N+6B+62+DYRK7DtgChb1vMeJ6jsVCOFTU"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9lk11IKYSwwbtv3ZPHZQTLrXrfmfbvbPbk2Q+Q+A9W"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAGm66rJsr8vjRCYDkH4lEPncPq27o6BHzpmRmkzOiM"
     ];
     description = "admin";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
@@ -77,90 +62,10 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # enable influxdb
-  services.influxdb2.enable = true;
-
-  # enable grafana
-  services.grafana = {
-    enable = true;
-    # port     = 3000;
-    addr = "0.0.0.0";
-    protocol = "http";
-    dataDir = "/var/lib/grafana";
-  };
-  # enable telegraf to log myself!
-  services.telegraf = {
-    enable = true;
-    extraConfig = {
-      inputs = {
-        docker = {
-          endpoint = "unix:///var/run/docker.sock";
-          gather_services = false;
-          container_name_include = [ ];
-          container_name_exclude = [ ];
-          timeout = "5s";
-          docker_label_include = [ ];
-          docker_label_exclude = [ ];
-          perdevice = true;
-          total = false;
-        };
-        internet_speed = { enable_file_download = true; };
-        disk = {
-          ignore_fs = [
-            "tmpfs"
-            "devtmpfs"
-            "devfs"
-            "iso9660"
-            "overlay"
-            "aufs"
-            "squashfs"
-          ];
-        };
-        diskio = { };
-        mem = { };
-        cpu = {
-          ## Whether to report per-cpu stats or not
-          percpu = true;
-          ## Whether to report total system cpu stats or not
-          totalcpu = true;
-          ## If true, collect raw CPU time metrics
-          collect_cpu_time = false;
-          ## If true, compute and report the sum of all non-idle CPU states
-          report_active = false;
-        };
-
-        # minecraft = {
-        #   server = "";
-        #   port = "25575";
-        #   password = "$MINECRAFT_PASSWORD"
-        # }
-      };
-      outputs = {
-        influxdb_v2 = {
-          urls = [ "http://127.0.0.1:8086" ];
-
-          ## Token for authentication.
-          token = "$INFLUX_TOKEN";
-
-          ## Organization is the name of the organization you wish to write to; must exist.
-          organization = "arouzing";
-
-          ## Destination bucket to write into.
-          bucket = "arouzingBucket";
-        };
-      };
-    };
-    ####
-
-    environmentFiles = [ "/etc/telegraf.env" ];
-  };
-  # must add telegraf to docker group
-  users.users.telegraf = { extraGroups = [ "docker" ]; };
-
   # Open ports in the firewall.
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 8086 3000 ];
+    allowedTCPPorts = [ 22 80 443 ];
     allowedUDPPorts = [ ];
   };
 
