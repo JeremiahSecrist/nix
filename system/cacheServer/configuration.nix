8c3ecf55bd8f48201e25f9a22b345370a3f51234
arouzing <owner@arouzing.xyz>
2022-08-07 20:38:57 -0400

    a<s

--- system/cacheServer/configuration.nix
+++ system/cacheServer/configuration.nix
@@ -34,8 +34,8 @@
       server = "https://acme-staging-v02.api.letsencrypt.org/directory";
     };
     acceptTerms = true;
-    certs."local.arouzing.win" = {
-      extraDomainNames = [ "cache.local.arouzing.win" ];
+    certs."cache.local.arouzing.win" = {
+      # extraDomainNames = [ "cache.local.arouzing.win" ];
       # dnsResolver = "1.1.1.1:53";
       dnsProvider = "cloudflare";
       credentialsFile = "/var/cf-token";
