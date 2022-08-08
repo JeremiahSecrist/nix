29a3ac0ea1b2d81b8536b1722c3c6e27401185fe
arouzing <owner@arouzing.xyz>
2022-08-07 20:37:29 -0400

    a

--- system/cacheServer/configuration.nix
+++ system/cacheServer/configuration.nix
@@ -39,7 +39,7 @@
       # dnsResolver = "1.1.1.1:53";
       dnsProvider = "cloudflare";
       credentialsFile = "/var/cf-token";
-      dnsPropagationCheck = false;
+      # dnsPropagationCheck = false;
     };
 
   };
