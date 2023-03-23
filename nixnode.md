a1238a43fdcc061032c5cbf658e3dd302e57c0c5
Arouzing <owner@arouzing.xyz>
2023-03-23 20:41:18 +0000

    Attempts to remove non nix methodology

--- nixnode.md
+++
@@ -1,5 +0,0 @@
-# Useful notes for nix language
-
-## Escaping varriable
-for single line `\${a}`
-for multiline `''${a}`
--- shell.nix
+++
@@ -1,8 +0,0 @@
-with (import <nixpkgs> {});
-mkShell {
-  shellHook = '' 
-  '';
-  buildInputs = [
-    gnumake
-  ];
-}
