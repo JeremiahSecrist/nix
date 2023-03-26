36dfe87cd1ab8fee48a9411b09511f2798e32157
Arouzing <owner@arouzing.xyz>
2023-03-26 07:16:19 +0000

    fixed improper boot local?

--- modules/nixos/disks/encryptedBoot.nix
+++ modules/nixos/disks/encryptedBoot.nix
@@ -8,10 +8,17 @@
     efi.canTouchEfiVariables = true;
     timeout = 1;
   };
-  boot.initrd.luks.devices.luksroot = {
-    #name = "root";
-    device = "/dev/sda";
-    preLVM = true;
-    allowDiscards = true;
+  boot.luks.devices = {
+	  crypted = {
+	    device = "/dev/sda3";
+	    preLVM = true;
+      allowDiscards = true;
+	  };
   };
+  # boot.initrd.luks.devices.luksroot = {
+  #   #name = "root";
+  #   device = "/dev/sda";
+  #   preLVM = true;
+  #   allowDiscards = true;
+  # };
 }
