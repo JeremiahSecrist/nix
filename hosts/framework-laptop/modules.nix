67d47b10ecc94ae7ebc51b19f290fb06c89997ab
Arouzing <owner@arouzing.xyz>
2023-03-26 06:28:17 +0000

    must look ugly

--- hosts/framework-laptop/modules.nix
+++ hosts/framework-laptop/modules.nix
@@ -1,29 +1,25 @@
 { config, ... }:
-
-  let 
-    modulesPath = ../../modules;
-  in
   {
     import = [
-      modulesPath + /nixos/desktop/gnome.nix
-      modulesPath + /nixos/disks/encryptedBoot.nix { installDir = "/dev/sda"; }
-      modulesPath + /nixos/kernel/sysctl.nix
-      modulesPath + /nixos/networking/nm.nix
-      modulesPath + /nixos/networking/tailscale.nix
-      modulesPath + /nixos/nix/flakes.nix
-      modulesPath + /nixos/nix/garbage-collection.nix
-      modulesPath + /nixos/nix/unfree.nix
-      modulesPath + /nixos/packages/base.nix
-      modulesPath + /nixos/packages/flatpak.nix
-      modulesPath + /nixos/performance/cpufreq.nix
-      modulesPath + /nixos/performance/power-management.nix
-      modulesPath + /nixos/performance/zram.nix
-      modulesPath + /nixos/regional/locale.nix
-      modulesPath + /nixos/security/yubikey.nix
-      modulesPath + /nixos/security/u2fLogin.nix
-      modulesPath + /nixos/shell/zsh.nix
-      modulesPath + /nixos/virtualization/docker.nix
-      modulesPath + /nixos/virtualization/libvirt.nix
-      modulesPath + /users/sky.nix
+      ../../modules/nixos/desktop/gnome.nix
+      ../../modules/nixos/disks/encryptedBoot.nix { installDir = "/dev/sda"; }
+      ../../modules/nixos/kernel/sysctl.nix
+      ../../modules/nixos/networking/nm.nix
+      ../../modules/nixos/networking/tailscale.nix
+      ../../modules/nixos/nix/flakes.nix
+      ../../modules/nixos/nix/garbage-collection.nix
+      ../../modules/nixos/nix/unfree.nix
+      ../../modules/nixos/packages/base.nix
+      ../../modules/nixos/packages/flatpak.nix
+      ../../modules/nixos/performance/cpufreq.nix
+      ../../modules/nixos/performance/power-management.nix
+      ../../modules/nixos/performance/zram.nix
+      ../../modules/nixos/regional/locale.nix
+      ../../modules/nixos/security/yubikey.nix
+      ../../modules/nixos/security/u2fLogin.nix
+      ../../modules/nixos/shell/zsh.nix
+      ../../modules/nixos/virtualization/docker.nix
+      ../../modules/nixos/virtualization/libvirt.nix
+      ../../modules/users/sky.nix
     ];
   }
