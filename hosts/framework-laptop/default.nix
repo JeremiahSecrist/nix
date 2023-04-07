73f576b019e6bbe5cd39cfa559b2a16ca2cd60ec
Arouzing <owner@arouzing.xyz>
2023-04-07 15:55:24 -0400

    migrated to new import system

--- hosts/framework-laptop/default.nix
+++ hosts/framework-laptop/default.nix
@@ -1,20 +1,34 @@
-{ self, lib, system, nixos-hardware, home-manager, disko, ... }:
-lib.nixosSystem {
-  inherit system;
+{ system, inputs, ... }:
+let 
+pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
+
+in
+inputs.nixpkgs.lib.nixosSystem {
+  
+  inherit system; #= "x86_64-linux";
+  specialArgs = {inherit inputs;};
   modules = [
+    ({
+
+      environment.systemPackages =  [
+        inputs.devenv.packages.${pkgs.system}.devenv
+      ];
+    }
+    )
     ./configuration.nix
     ./hardware.nix
     ./modules.nix
     ./packages.nix
-    nixos-hardware.nixosModules.framework-12th-gen-intel
-    disko.nixosModules.disko
-    home-manager.nixosModules.home-manager
+    inputs.nixos-hardware.nixosModules.framework-12th-gen-intel
+    inputs.disko.nixosModules.disko
+    inputs.home-manager.nixosModules.home-manager
     {
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
       home-manager.users.sky = {
         imports = [ 
           ../../modules/home-manager/sky/home.nix
+          ../../modules/home-manager/sky/laptop/dconf.nix
         ];
       };
     }
