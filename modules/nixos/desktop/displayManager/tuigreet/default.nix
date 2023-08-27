bc043b2f83e020f50aee84c49314c5372e5d7f49
Jermeiah S <owner@arouzing.xyz>
2023-08-27 15:21:42 -0400

    fix: greetd tty issue for tuigreet

--- modules/nixos/desktop/displayManager/tuigreet/default.nix
+++ modules/nixos/desktop/displayManager/tuigreet/default.nix
@@ -26,9 +26,22 @@ in {
         };
       };
     };
-
     systemd.tmpfiles.rules = [
       "d /var/cache/tuigreet/ 0755 greeter ${gduser} - -"
     ];
+    # this is a life saver.
+    # literally no documentation about this anywhere.
+    # might be good to write about this...
+    # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+    systemd.services.greetd.serviceConfig = {
+      Type = "idle";
+      StandardInput = "tty";
+      StandardOutput = "tty";
+      StandardError = "journal"; # Without this errors will spam on screen
+      # Without these bootlogs will spam on screen
+      TTYReset = true;
+      TTYVHangup = true;
+      TTYVTDisallocate = true;
+    };
   };
 }
