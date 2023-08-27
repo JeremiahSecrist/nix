ac0322c6f002d8a33df6ba290454ce10390231c6
Jermeiah S <owner@arouzing.xyz>
2023-08-27 18:06:14 -0400

    feat: toplevel trusted caches

--- flake.nix
+++ flake.nix
@@ -83,5 +83,17 @@
         shellHook = ''
         '';
       };
+      nixConfig = {
+        extra-trusted-public-keys = [
+          "laptop-deploy:OMe69aOGxkvIhEYIECd1U3CE/PAouObowS7W4nDS460="
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+        ];
+        builders-use-substitutes = true;
+         extra-substituters = [
+          "https://anyrun.cachix.org"
+          "https://cache.nixos.org"
+        ];
+      };
     };
 }
