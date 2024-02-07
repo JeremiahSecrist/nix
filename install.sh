03519c299f840683a57a57132c58568ac316afa5
Jeremiah S <owner@arouzing.xyz>
2024-02-07 17:45:45 +0000

    swapSize

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 cp ./modules/nixos/disks/impermanence.nix /tmp/disko.nix
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/refs/tags/v1.3.0 -- --mode $3 /tmp/disko.nix --arg device '"$1"' --arg device '"$2"'
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/refs/tags/v1.3.0 -- --mode $3 /tmp/disko.nix --arg device '"$1"' --arg swapSize '"$2"'
 nixos-install --flake .\#$4
