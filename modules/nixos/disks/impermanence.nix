61298e10814e741256053eb3b4cb95a4dbe2a56a
Jeremiah S <owner@arouzing.xyz>
2024-02-07 12:04:00 -0500

    Update impermanence.nix

--- modules/nixos/disks/impermanence.nix
+++ modules/nixos/disks/impermanence.nix
@@ -10,14 +10,14 @@
       content = {
         type = "gpt";
         partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
+          #boot = {
+           # name = "boot";
+            #size = "1M";
+            #type = "EF02";
+          #};
           esp = {
             name = "ESP";
-            size = "500M";
+            size = "512M";
             type = "EF00";
             content = {
               type = "filesystem";
