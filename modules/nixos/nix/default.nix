48283894f360a9a912bffb444fe0493121fdd07b
Jermeiah S <owner@arouzing.xyz>
2024-02-08 17:25:30 -0500

    remove secret key

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -60,7 +60,7 @@ in {
           "https://cache.ngi0.nixos.org"
         ];
 
-        secret-key-files = /var/lib/nix-keys/deploy.secret;
+        # secret-key-files = /var/lib/nix-keys/deploy.secret;
         allowed-users = [
           "@wheel"
           "@builders"
