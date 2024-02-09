2a576234f8c954d6ac4800fad77e0cd7de49509a
Jermeiah S <owner@arouzing.xyz>
2024-02-09 00:22:59 -0500

    hardcode install

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 cp ./modules/nixos/disks/impermanence.nix /tmp/disko.nix
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/refs/tags/v1.3.0 -- --mode $3 /tmp/disko.nix --arg device $1 --arg swapSize $2
-nixos-install --flake .\#$4
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/refs/tags/v1.3.0 -- --mode $1 /tmp/disko.nix --arg device '"/dev/nvme0n1"' --arg swapSize '"8G"'
+nixos-install --flake .\#lappy
