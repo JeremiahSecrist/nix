459e80ed3d423fdec7d78494a8d19f15e03c9250
Jermeiah S <owner@arouzing.xyz>
2024-02-15 22:25:08 -0500

    Testing SSH signing

--- users/sky/default.nix
+++ users/sky/default.nix
@@ -3,7 +3,7 @@
   pkgs,
   ...
 }: let
-myPubKey = "${pkgs.writeText "mykey.pub" "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBA9i9HoP7X8Ufzz8rAaP7Nl3UOMZxQHMrsnA5aEQfpTyIQ1qW68jJ4jGK5V6Wv27MMc3czDU1qfFWIbGEWurUHQ="}";
+myPubKey = "${pkgs.writeText "mykey.pub" "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBA9i9HoP7X8Ufzz8rAaP7Nl3UOMZxQHMrsnA5aEQfpTyIQ1qW68jJ4jGK5V6Wv27MMc3czDU1qfFWIbGEWurUHQ=\n"}";
 in{
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
